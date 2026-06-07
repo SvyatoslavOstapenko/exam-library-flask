b57421f55ba1e06e8d982acfe2c7f19d347bf221
MpuPoli <svyat.ostapenko14@bk.ru>
2026-06-07 21:06:35 +0300

    Add WSGI entry point

---
+++ wsgi.py
@@ -0,0 +1,15 @@
+import os
+import sys
+
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+APP_DIR = os.path.join(BASE_DIR, 'app')
+
+if APP_DIR not in sys.path:
+    sys.path.insert(0, APP_DIR)
+
+from app import app as flask_app
+
+
+app = flask_app
+application = flask_app
