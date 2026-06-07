264d42fd8c4d57d1184c8bd7274b4a86563ba59d
MpuPoli <svyat.ostapenko14@bk.ru>
2026-06-07 20:38:29 +0300

    Update app and auth for library project

--- app/app.py
+++ app/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, send_from_directory
 from flask_migrate import Migrate
 from sqlalchemy.exc import SQLAlchemyError
-from models import db, Category, Image
+
+from models import db, Book, Cover
 from auth import bp as auth_bp, init_login_manager
-from courses import bp as courses_bp
+from books import bp as books_bp
+
 
 app = Flask(__name__)
 application = app
@@ -15,25 +17,36 @@ migrate = Migrate(app, db)
 
 init_login_manager(app)
 
+
 @app.errorhandler(SQLAlchemyError)
 def handle_sqlalchemy_error(err):
-    error_msg = ('Возникла ошибка при подключении к базе данных. '
-                 'Повторите попытку позже.')
+    error_msg = (
+        'Возникла ошибка при подключении к базе данных. '
+        'Повторите попытку позже.'
+    )
     return f'{error_msg} (Подробнее: {err})', 500
 
+
 app.register_blueprint(auth_bp)
-app.register_blueprint(courses_bp)
+app.register_blueprint(books_bp)
+
 
 @app.route('/')
 def index():
-    categories = db.session.execute(db.select(Category)).scalars()
+    books = db.session.execute(
+        db.select(Book).order_by(Book.year.desc())
+    ).scalars()
+
     return render_template(
         'index.html',
-        categories=categories,
+        books=books,
     )
 
-@app.route('/images/<image_id>')
-def image(image_id):
-    img = db.get_or_404(Image, image_id)
-    return send_from_directory(app.config['UPLOAD_FOLDER'],
-                               img.storage_filename)
+
+@app.route('/covers/<int:cover_id>')
+def cover(cover_id):
+    cover_obj = db.get_or_404(Cover, cover_id)
+    return send_from_directory(
+        app.config['UPLOAD_FOLDER'],
+        cover_obj.storage_filename
+    )
---
+++ app/books.py
@@ -0,0 +1,3 @@
+from flask import Blueprint
+
+bp = Blueprint('books', __name__, url_prefix='/books')
