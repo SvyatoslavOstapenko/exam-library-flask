22eb8f975ff9948d4d18226aed77ef26736823a4
MpuPoli <svyat.ostapenko14@bk.ru>
2026-06-07 20:40:05 +0300

    Add helpers for markdown and covers

--- app/tools.py
+++ app/tools.py
@@ -1,53 +1,108 @@
 import hashlib
-import uuid
 import os
-from werkzeug.utils import secure_filename
+
+import bleach
+import markdown
 from flask import current_app
-from models import db, Course, Image
+from markupsafe import Markup
+from werkzeug.utils import secure_filename
 
-class CoursesFilter:
-    def __init__(self, name, category_ids):
-        self.name = name
-        self.category_ids = category_ids
-        self.query = db.select(Course)
+from models import db, Cover
 
-    def perform(self):
-        self.__filter_by_name()
-        self.__filter_by_category_ids()
-        return self.query.order_by(Course.created_at.desc())
 
-    def __filter_by_name(self):
-        if self.name:
-            self.query = self.query.filter(
-                Course.name.ilike('%' + self.name + '%'))
+ALLOWED_TAGS = list(bleach.sanitizer.ALLOWED_TAGS) + [
+    'p', 'br', 'pre', 'code', 'h1', 'h2', 'h3',
+    'h4', 'h5', 'h6', 'ul', 'ol', 'li',
+    'strong', 'em', 'blockquote'
+]
 
-    def __filter_by_category_ids(self):
-        if self.category_ids:
-            self.query = self.query.filter(
-                Course.category_id.in_(self.category_ids))
+ALLOWED_ATTRIBUTES = {
+    **bleach.sanitizer.ALLOWED_ATTRIBUTES,
+    'a': ['href', 'title'],
+}
 
-class ImageSaver:
-    def __init__(self, file):
-        self.file = file
 
-    def save(self):
-        self.img = self.__find_by_md5_hash()
-        if self.img is not None:
-            return self.img
-        file_name = secure_filename(self.file.filename)
-        self.img = Image(
-            id=str(uuid.uuid4()),
-            file_name=file_name,
-            mime_type=self.file.mimetype,
-            md5_hash=self.md5_hash)
-        self.file.save(
-            os.path.join(current_app.config['UPLOAD_FOLDER'],
-                         self.img.storage_filename))
-        db.session.add(self.img)
-        db.session.commit()
-        return self.img
+def sanitize_text(text):
+    return bleach.clean(
+        text or '',
+        tags=ALLOWED_TAGS,
+        attributes=ALLOWED_ATTRIBUTES,
+        strip=True,
+    )
 
-    def __find_by_md5_hash(self):
-        self.md5_hash = hashlib.md5(self.file.read()).hexdigest()
-        self.file.seek(0)
-        return db.session.execute(db.select(Image).filter(Image.md5_hash == self.md5_hash)).scalar()
+
+def markdown_to_html(text):
+    html = markdown.markdown(
+        text or '',
+        extensions=['extra', 'nl2br'],
+    )
+
+    safe_html = bleach.clean(
+        html,
+        tags=ALLOWED_TAGS,
+        attributes=ALLOWED_ATTRIBUTES,
+        strip=True,
+    )
+
+    return Markup(safe_html)
+
+
+def calculate_md5(file):
+    md5_hash = hashlib.md5(file.read()).hexdigest()
+    file.seek(0)
+    return md5_hash
+
+
+def save_cover(file, book_id):
+    os.makedirs(current_app.config['UPLOAD_FOLDER'], exist_ok=True)
+
+    md5_hash = calculate_md5(file)
+
+    existing_cover = db.session.execute(
+        db.select(Cover).filter_by(md5_hash=md5_hash)
+    ).scalar()
+
+    file_name = secure_filename(file.filename)
+
+    if existing_cover:
+        file_name = existing_cover.file_name
+
+    cover = Cover(
+        file_name=file_name,
+        mime_type=file.mimetype,
+        md5_hash=md5_hash,
+        book_id=book_id,
+    )
+
+    db.session.add(cover)
+    db.session.flush()
+
+    file_path = os.path.join(
+        current_app.config['UPLOAD_FOLDER'],
+        cover.storage_filename,
+    )
+
+    if not os.path.exists(file_path):
+        file.save(file_path)
+
+    return cover
+
+
+def delete_cover_file(cover):
+    if not cover:
+        return
+
+    same_files_count = db.session.execute(
+        db.select(db.func.count(Cover.id)).filter_by(md5_hash=cover.md5_hash)
+    ).scalar()
+
+    if same_files_count and same_files_count > 1:
+        return
+
+    file_path = os.path.join(
+        current_app.config['UPLOAD_FOLDER'],
+        cover.storage_filename,
+    )
+
+    if os.path.exists(file_path):
+        os.remove(file_path)
